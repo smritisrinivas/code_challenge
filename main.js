a52f2296cba6c36dda3df5ae268b9593106386be
smritisrinivas <30934510+smritisrinivas@users.noreply.github.com>
2020-11-23 14:30:58 +0100

    Update main.js
    Removed Redundancy

--- main.js
+++ main.js
@@ -7,13 +7,16 @@ const data = require('./chargingStations.json');
 var router = Router();
 router.get('/', function(req, res) {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    res.end('Welcome at Green Globe !! \n\nTo know more about the current store please follow this path: /tenant/greenglobe/store/bangalore, \nTo check the status of the charging stations at your desired time of the day follow this path: /tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/status/:time, \nIn order to get the detailed information about the next events for the day please follow this path: /tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/nextevent/:time');
+    res.end('Welcome at Green Globe !! \n\nTo know more about the current store please follow this path: /tenant/greenglobe/store/bangalore \nTo check the status of the charging stations at your desired time of the day follow this path: /tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/status/:time \nIn order to get the detailed information about the next events for the day please follow this path: /tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/nextevent/:time');
 })
+
+/* Store Information */
 router.get('/tenant/greenglobe/store/bangalore', function(req, res) {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     res.end('Information page Green Globe, Bangalore \n\nGreen Globe, the leading super market chain since 1995. From apparel to farm fresh Veggies you can buy everything at Green Globe. To be informed of the discounts at Green Globe Bangalore subscribe to our newsletter. \n\nWhile you shop now you may charge your vehicle at Green Globe Bangalore, we now have charging stations for your vehicle. \n\nfollow this path to know the status of the charging stations for the current day: /tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/status/:time \nIn order to get the detailed information about the next events for the day please follow this path: /tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/nextevent/:time');
 })
 
+/* Get the status of the chargingstation */
 router.get('/tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/status/:time', function(req, res) {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     var response = 'Welcome at Green Globe charging service. \n\nAt the moment we have 2 charging stations at our store. \n\nIn order to check the status of your desired charging station for the current day, simply enter the charging station number (either 1 or 2) as well as the time (in 24-hour notation) that you wish to charge your vehicle in the path ex:/tenant/greenglobe/store/bangalore/chargingstation/1/status/0800 . \n\ncharging station that you are requesting for: ' + req.params.chargingstation + ' \nYour desired time: ' + req.params.time + '\nStatus: ';
@@ -41,6 +44,7 @@ router.get('/tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/
     res.end(response + " " + isOpen + checkNextEvent);
 })
 
+/* Get the next event with time stamp */
 router.get('/tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/nextevent/:time', function(req, res) {
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
     var response = 'Green Globe Bangalore Charging service !! \n\nThank you for your interest in getting a deatiled information of the next events for the current day. \n\nIn order to check the next event of your desired charging station for the current day simply enter the charging station number (either 1 or 2) as well as the time (in 24-hour notation) that you wish to charge your vehicle in the path ex: /tenant/greenglobe/store/bangalore/chargingstation/1/nextevent/0800 . \n\ncharging station that you are requesting for: ' + req.params.chargingstation + ' \n\nYour desired time: ' + req.params.time + '\n\nNext event: ';
@@ -56,9 +60,7 @@ router.get('/tenant/greenglobe/store/bangalore/chargingstation/:chargingstation/
             var ChargingStationsTime = chargingStations[i].open_hours[day].times;
             for (var j = 0; j < ChargingStationsTime.length; j++) {
                 var chargingStationOpen = ChargingStationsTime[j].open;
-                var chargingStationClose = ChargingStationsTime[j].close;
-                var chargingStationOpen = ChargingStationsTime[j].open;
-                var chargingStationClose = ChargingStationsTime[j].close;
+                var chargingStationClose = ChargingStationsTime[j].close;               
                 if (chargingStationOpen - acceptedTime > 0) {
                     var nextEvent = 'opening at ' + chargingStationOpen;
                     break;
